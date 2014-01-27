3b85522469f27ba74cf1c6cd530b503934b36f02
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-01-27 16:18:20 +0100

    Normalized comments and prepared the first ruby gem

---
+++ lib/SassyCast.rb
@@ -0,0 +1,14 @@
+require 'compass'
+extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+Compass::Frameworks.register('SassyCast', :path => extension_path)
+
+#  Version is a number. If a version contains alphas, it will be created as a prerelease version
+#  Date is in the form of YYYY-MM-DD
+module SassyCast
+  VERSION = "0.0.1"
+  DATE = "2014-01-27"
+end
+
+module Sass::Script::Functions
+
+end
