77d3e8ed03befbf66e2f584a42a2fdb0ada24f2c
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-01-31 11:57:56 +0100

    Bump 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
 # Changelog
 
+* `1.0.0`: `to-list` improvements, stable API
 * `0.0.1`: initial commit
--- lib/SassyCast.rb
+++ lib/SassyCast.rb
@@ -5,8 +5,8 @@ Compass::Frameworks.register('SassyCast', :path => extension_path)
 #  Version is a number. If a version contains alphas, it will be created as a prerelease version
 #  Date is in the form of YYYY-MM-DD
 module SassyCast
-  VERSION = "0.0.1"
-  DATE = "2014-01-27"
+  VERSION = "1.0.0"
+  DATE = "2014-01-31"
 end
 
 module Sass::Script::Functions
